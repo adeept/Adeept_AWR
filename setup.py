559c05a27cf1e8e55fd508e847adad9801a407d7
Adeept <support@adeept.com>
2019-04-24 13:14:32 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,7 +7,18 @@
 # Date        : 2018/10/12
 
 import os
-import time
+import sys
+
+
+def search(path,name):
+    for root, dirs, files in os.walk(path):
+        if name in dirs or name in files:
+            flag = 1
+            root = str(root)
+            dirs = str(dirs)
+            return os.path.join(root, dirs)
+    return -1
+
 
 def replace_num(file,initial,new_num):  
     newline=""
@@ -45,13 +56,21 @@ for x in range(1,4):
 	if os.system("sudo pip3 install rpi_ws281x") == 0:
 		break
 
+for x in range(1,4):
+	if os.system("sudo apt-get install python3-smbus") == 0:
+		break
+
+for x in range(1,4):
+	if os.system("sudo pip3 install mpu6050-raspberrypi") == 0:
+		break
+
 try:
 	replace_num("/boot/config.txt",'#dtparam=i2c_arm=on','dtparam=i2c_arm=on\nstart_x=1\n')
 except:
 	print('try again')
 
 for x in range(1,4):
-	if os.system("sudo pip3 install -U pip") == 0:
+	if os.system("sudo pip3 install -U pip setuptools wheel") == 0:
 		break
 
 for x in range(1,4):
@@ -99,7 +118,7 @@ for x in range(1,4):
 		break
 
 try:
-	os.system("sudo cd //home/pi/adeept_awr/create_ap && sudo make install")
+	os.system("sudo cd //home/pi/adeept_darkpaw/create_ap && sudo make install")
 except:
 	pass
 
@@ -107,15 +126,20 @@ for x in range(1,4):
 	if os.system("sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq") == 0:
 		break
 
+path_get = str(search('//home/pi/','server.py'))
+path_get=path_get[:-15]
+
 try:
+	try:
+		os.system('sudo rm -rf //home/pi/.config/autostart')
+	except:
+		pass
 	os.system('sudo mkdir //home/pi/.config/autostart')
 	os.system('sudo touch //home/pi/.config/autostart/car.desktop')
 	with open("//home/pi/.config/autostart/car.desktop",'w') as file_to_write:
-		file_to_write.write("[Desktop Entry]\n   Name=Car\n   Comment=Car\n   Exec=sudo python3 //home/pi/adeept_awr/server/server.py\n   Icon=false\n   Terminal=false\n   MutipleArgs=false\n   Type=Application\n   Catagories=Application;Development;\n   StartupNotify=true")
+		file_to_write.write("[Desktop Entry]\n   Name=Car\n   Comment=Car\n   Exec=sudo python3 %sserver.py\n   Icon=false\n   Terminal=false\n   MutipleArgs=false\n   Type=Application\n   Catagories=Application;Development;\n   StartupNotify=true"%path_get)
 except:
 	pass
-
-os.system("sudo cp -f //home/pi/adeept_awr/server/config.txt //home/pi/config.txt")
 
 print('restarting')
 
